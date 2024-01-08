fd62a88536609a39ec2563204959c6b682177510
JoshBenn <jdbenn@asu.edu>
2024-01-07 17:04:17 -0700

    Added s! and cnct! macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,1 +1,20 @@
 
+
+#[macro_export]
+macro_rules! s {
+    ($str:expr) => { String::from($str) };
+}
+
+#[macro_export]
+macro_rules! cnct {
+    ($e1:expr $(, $e2:expr)*) => {
+        {
+            let mut string: String = String::from($e1);
+            $(
+                let second: &str = &$e2;
+                string.push_str(second);
+            )+
+            string
+        }
+    };
+}
